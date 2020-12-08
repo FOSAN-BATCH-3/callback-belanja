c67953227eead5febbeac766cd6cf89631e6409e
Linux <rizki.st3004@gmail.com>
2020-12-08 18:42:19 +0700

    sesuai permintaan

--- index.js
+++ index.js
@@ -2,11 +2,39 @@
 //your Program in here
 let jajan = require ("./beli.js")
 
-jajan(5000000, {item: 'Tas', harga: 900000, waktu: 3000}, function(susuk) {
-    jajan(susuk, {item: 'Sepatu', harga: 1500000, waktu: 2000}, function(susuk) {
-        jajan(susuk, {item: 'Jam Tangan', harga: 2000000, waktu: 1500}, function(susuk) {
-            jajan(susuk, {item: 'Kacamata', harga: 500000, waktu: 2000}, function(susuk) {
-                jajan(susuk, {item: 'Handphone', harga: 3500000, waktu: 4000}, function() {
+let shopping = [
+    {
+        item: 'Tas',
+        harga: 900000,
+        waktu: 3000
+    },
+    {
+        item: 'Sepatu',
+        harga: 1500000,
+        waktu: 2000
+    },
+    {
+        item: 'Jam Tangan',
+        harga: 2000000,
+        waktu: 1500
+    },
+    {
+        item: 'Kacamata',
+        harga: 500000,
+        waktu: 2000
+    },
+    {
+        item: 'Handphone',
+        harga: 3500000,
+        waktu: 4000
+    }
+]
+
+jajan(5000000, shopping[0], function(susuk) {
+    jajan(susuk, shopping[1], function(susuk) {
+        jajan(susuk, shopping[2], function(susuk) {
+            jajan(susuk, shopping[3], function(susuk) {
+                jajan(susuk, shopping[4], function() {
                 })
             })
         })
