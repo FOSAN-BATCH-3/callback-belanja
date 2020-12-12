856c9f4edc88dcbc47abd03d0d6afb78195652df
Kidou07 <70023129+Kidou07@users.noreply.github.com>
2020-12-12 15:57:30 +0700

    done
    maaf kak kalo beli,jsnya kena modif soalnya bosen saya:v

--- beli.js
+++ beli.js
@@ -1,11 +1,18 @@
 function beli(uang, obj, cb){
   setTimeout(function(){
+    if(obj.item!="donasi"){
     console.log(`Saya pergi membeli ${obj.item}`)
-    let kembalian = uang - obj.harga
-    if (kembalian >= 0) {
+    }
+    let kembalian = uang - obj.harga*obj.jumlah
+    if (kembalian >= 0 && obj.item!="donasi") {
       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
       cb(kembalian)
-    }else{
+    }
+    else if(obj.item=="donasi"){
+      console.log(`Saya ber ${obj.item} uang kembaliannya ${kembalian}`);
+      cb(kembalian)
+    }
+    else{
       console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${uang}`);
       cb(uang)
     }
